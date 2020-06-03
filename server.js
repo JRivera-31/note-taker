2251d01e1e1b353de4a748b191e98d71d2e7ce1f
Josh R <jriver35@uncc.edu>
2020-06-03 15:25:57 -0400

    Added app.delete. Post works.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
 
 const app = express()
 const PORT = 8080
@@ -8,7 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-//Create Routes
+const notes = []
+
+// Create Routes
 // ============================
 // Home page route
 app.get("/", (req, res) => {
@@ -22,8 +25,41 @@ app.get("/notes", (req, res) => {
 
 // Display notes
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
+    res.sendFile(path.join(__dirname, "db/db.json"))
 })
+
+// Save new notes to the db
+app.post("/api/notes", (req, res) => {
+    // Assign the note to variable
+    let newNote = req.body
+
+    newNote.id = Math.floor(Math.random())
+
+    console.log(newNote.id)
+
+    // Push it to the notes array
+    notes.push(newNote)
+
+    // Write the array to the db
+    fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), err => {
+        if (err) throw err
+
+        console.log("Note has been written to the file..")
+        
+    })
+
+    // Return the notes array
+    res.send(notes)
+   
+})
+
+// Delete notes from the db
+app.delete("/api/notes/:notes", (req, res) => {
+    const noteID = req.params.notes
+    const removed = notes.splice(notes, noteID)
+    res.json(removed)
+})
+
 
 // App Listener
 app.listen(PORT, function() {
